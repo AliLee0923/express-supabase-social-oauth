d0df3c4f6614b510a12884ee563bffa8298659a5
Ali <ali.lee.0923@gmail.com>
2024-06-06 19:16:55 -0400

    youtube comment work

--- src/controllers/youtube.ts
+++ src/controllers/youtube.ts
@@ -123,7 +123,6 @@ export const postComment = async (req: Request, res: Response) => {
               textOriginal: comment,
             },
           },
-          isPublic: true,
         },
       },
       {

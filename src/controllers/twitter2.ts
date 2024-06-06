353c556bdf0698e7c0709283555fd91cad00b12f
Ali <ali.lee.0923@gmail.com>
2024-06-06 16:52:38 -0400

    token as token

--- src/controllers/twitter2.ts
+++ src/controllers/twitter2.ts
@@ -37,11 +37,12 @@ export const initiateTwitterOAuth = (req: Request, res: Response) => {
     {
       response_type: "code",
       client_id: TWITTER_CLIENT_ID,
-      redirect_uri: TWITTER_REDIRECT_URI + "?token=" + token,
+      redirect_uri: TWITTER_REDIRECT_URI,
       scope: "tweet.read tweet.write users.read offline.access",
       state: state,
       code_challenge: codeChallenge,
       code_challenge_method: "S256",
+      token: token
     }
   )}`;
 
